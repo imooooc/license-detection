4480a6d7d07bb7786366e92ac93ab6c4e1978b0f
Charles <imoocbook@163.com>
2020-03-27 14:23:02 +0800

    two project have been merged

--- server/vmoli/detection/models.py
+++ server/vmoli/detection/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Test(models.Model):
+	nickname = models.CharField(max_length=20)
+
+
+
+class ImageModel(models.Model):
+	image = models.ImageField(upload_to="image/%Y/%m/%d", blank=True)
---
+++ server/vmoli/detection/urls.py
@@ -0,0 +1,1 @@
+from django.urls import pathfrom .views import *urlpatterns = [    path('', IndexView.as_view(), name='index'),    path('upload/', Uploads.as_view(), name='uploads'),]
