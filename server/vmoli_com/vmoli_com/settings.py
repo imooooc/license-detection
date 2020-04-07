1945549168341fbe2d2c7aaf8377ba0eddeca6c5
Charles <imoocbook@163.com>
2020-04-07 16:59:48 +0800

    settings.py

--- server/vmoli_com/vmoli_com/settings.py
+++ server/vmoli_com/vmoli_com/settings.py
@@ -129,12 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/com_static'),
+    os.path.join(BASE_DIR, 'com_static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = '/static/com_static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
