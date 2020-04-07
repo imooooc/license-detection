08e20407539b1ae20199bb0600daef178ab83793
Charles <imoocbook@163.com>
2020-04-07 16:11:22 +0800

    fixed the bug of static files loading failure

--- server/vmoli_com/vmoli_com/settings.py
+++ server/vmoli_com/vmoli_com/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vmoli',
-        'USER': 'ubuntu',
-        'PASSWORD': '',
+        'USER': 'root',
+        'PASSWORD': 'npwd',
         'HOST': '127.0.0.1',
         'PORT': 3306,
     }
@@ -129,12 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/com_static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/com_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
